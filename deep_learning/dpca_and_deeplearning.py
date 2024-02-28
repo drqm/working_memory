acf250295201d4c809fd80d2ddcc70592680934a
Misddye <737323321@qq.com>
2024-02-28 21:47:24 +0800

    A sorted version with a few omments

--- deep_learning/dpca_and_deeplearning.py
+++ deep_learning/dpca_and_deeplearning.py
@@ -10,40 +10,35 @@ import pickle
 from dpca_calculation import dpca_fit
 
 subject = '0011_U7X'
-# recall_array_ls, manual_array_ls = get_subject_array_ls(subject)
-# with open('data_arrays.pkl', 'wb') as f:
-#     pickle.dump((recall_array_ls, manual_array_ls), f)
+recall_array_ls, manual_array_ls = get_subject_array_ls(subject)
+with open('data_arrays.pkl', 'wb') as f:
+    pickle.dump((recall_array_ls, manual_array_ls), f)
 with open('data_arrays.pkl', 'rb') as f:
     recall_array_ls, manual_array_ls = pickle.load(f) # [(30, 306, 501), (29, 306, 501)] + [(30, 306, 501), (30, 306, 501)]
 
 data_arrays = recall_array_ls + manual_array_ls
 
-# 计算最小的实验数
+# reshape data for dpca
 min_experiment_count = min([i.shape[0] for i in data_arrays])
-
-# 更新矩阵形状，使用最小实验数
-new_matrix_shape = [min_experiment_count, 306, 2, 2, 501]
+new_matrix_shape = [min_experiment_count, 306, 2, 2, 501] # [29, 306, 2, 2, 501]
 new_matrix = np.zeros(new_matrix_shape)
 
 for i in range(2):
     for j in range(2):
         new_matrix[:, :, i, j, :] = data_arrays[i*2+j][:min_experiment_count]
 
-# 生成第一维的索引，用于后续的训练和测试集分割
+# split train test data
 experiment_indices = np.arange(min_experiment_count)
+train_indices, test_indices = train_test_split(experiment_indices, test_size=0.3)
 
-# 随机分组，这次包括生成索引的代码
-train_indices, test_indices = train_test_split(experiment_indices, test_size=0.3, random_state=42)
-
-# 使用索引分割新矩阵为训练和测试集
-train_matrix = new_matrix[train_indices]#(20, 306, 2, 2, 501)
-test_matrix = new_matrix[test_indices]
+train_matrix = new_matrix[train_indices] #(20, 306, 2, 2, 501)
+test_matrix = new_matrix[test_indices] #(9, 306, 2, 2, 501)
 
 n_components = 5
 Z, dpca = dpca_fit(train_matrix, 'bst', n_components)
 
 def fit_and_sort_array(train_matrix):
-    new_array = np.zeros([len(train_matrix),7*n_components,2,2,501])
+    new_array = np.zeros([len(train_matrix),7*n_components,2,2,501]) #[20,7*5,2,2,501]
     for i in range(len(train_matrix)):
         a = dpca.transform(train_matrix[i])
 
@@ -52,6 +47,7 @@ def fit_and_sort_array(train_matrix):
             new_array[i][ncount*5:ncount*5+5] = a[j]
             ncount = ncount + 1
 
+    # [20*2*2,7*5,501] --> back to n_trials, n_channels, n_times again for training
     new_array = new_array.reshape(len(train_matrix)*2*2, 7*n_components, 501)
     print(new_array.shape)
     labels = np.tile(np.arange(4), len(train_matrix))
@@ -61,12 +57,10 @@ def fit_and_sort_array(train_matrix):
 X_train, y_train = fit_and_sort_array(train_matrix)
 X_test, y_test = fit_and_sort_array(test_matrix)
 
-# for i in range(len(X_test)):
-#     print(X_train[0] == [X_train[i]])
-
 X_train = X_train[:, None, :, 100:]
 X_test = X_test[:, None, :, 100:]
 
+#Standardize
 mean = X_train.mean()
 std = X_train.std()
 X_train = (X_train - mean) / std
@@ -84,7 +78,6 @@ with open('data_loader.pkl', 'wb') as f:
     pickle.dump((train_loader, test_loader), f)
 with open('data_loader.pkl', 'rb') as f:
     train_loader, test_loader = pickle.load(f)
-
 
 model = Conformer()  # model choose comformer
 
@@ -113,8 +106,8 @@ def tes(model, device, test_loader, criterion_cls):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             _, output = model(data)
-            print('output,',output)
-            print('target,',target)
+            # print('output,',output)
+            # print('target,',target)
             test_loss += criterion_cls(output, target).item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
